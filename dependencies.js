a0e9dade86ae8bf8bd734ec3366e8254e0e03043
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-05-18 14:00:12 -0300

    feat: :memo:

--- dependencies.js
+++ dependencies.js
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 const path = require("path");
 console.log("===Instalando dependencias===");
 // Use the space inside execute to add the necessary dependencies.
+// The example dependency is the mocha testing framework, which will be installed when running this program. If not, just configure your exec by installig or uninstalling the necessary dependencies.
 exec(`npm init -y && npm install -save-dev mocha --prefix ${path.resolve(process.cwd())}`, (error, stdout, stderr) => {
   if(error) {
     console.log(`error: ${error.message}`);
